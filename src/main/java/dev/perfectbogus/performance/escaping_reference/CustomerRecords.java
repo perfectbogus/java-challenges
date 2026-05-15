3c5e9f035d24f6befe1d55a5997e0edc1dacf571
perfectbogus <war_army_10@hotmail.com>
2026-05-15 16:03:22 -0600

    Section 7 Chapter 30

---
+++ src/main/java/dev/perfectbogus/performance/escaping_reference/CustomerRecords.java
@@ -0,0 +1,23 @@
+package dev.perfectbogus.performance.escaping_reference;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+public class CustomerRecords implements Iterable<Customer> {
+
+    private Map<String, Customer> records;
+
+    public CustomerRecords() {
+        this.records = new HashMap<>();
+    }
+
+    public void addCustomer(Customer c) {
+        this.records.put(c.getName(), c);
+    }
+
+    @Override
+    public Iterator<Customer> iterator() {
+        return records.values().iterator();
+    }
+}
---
+++ src/main/java/dev/perfectbogus/performance/escaping_reference/Main.java
@@ -0,0 +1,28 @@
+package dev.perfectbogus.performance.escaping_reference;
+
+import java.util.Iterator;
+
+public class Main {
+
+    static void main(String[] args) {
+        CustomerRecords records = new CustomerRecords();
+
+        records.addCustomer(new Customer("Jonh"));
+        records.addCustomer(new Customer("Simon"));
+
+        System.out.println("All records");
+        for (Customer next : records) {
+            System.out.println(next);
+        }
+
+        // Even with iterator is possible to modify the reference
+        Iterator<Customer> it = records.iterator();
+        it.next();
+        it.remove();
+
+        System.out.println("modified records:");
+        for (Customer next : records) {
+            System.out.println(next);
+        }
+    }
+}
