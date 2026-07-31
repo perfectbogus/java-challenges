496ec136badb0cf745f2bcfb5d4c01cd8780d265
perfectbogus <war_army_10@hotmail.com>
2026-07-31 15:56:39 -0600

    LeetCode Graphs: Nearest Exit

---
+++ src/main/java/dev/perfectbogus/leetcode/graphs/NearestExit.java
@@ -0,0 +1,51 @@
+package dev.perfectbogus.leetcode.graphs;
+
+import dev.perfectbogus.leetcode.binary.tree.dfs.TreeNode;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class NearestExit {
+
+    public static void main(String[] args) {
+        char[][] maze = {{'+','+','.','+'},{'.','.','.','+'},{'+','+','+','.'}};
+
+        System.out.println(nearestExit(maze, new int[]{1, 2}));
+    }
+
+    public static int nearestExit(char[][] maze, int[] entrance) {
+        int rows = maze.length;
+        int columns = maze[0].length;
+
+        Queue<int[]> queue = new LinkedList<>();
+        queue.offer(entrance);
+        maze[entrance[0]][entrance[1]] = '+';
+
+        int[][] directions = new int[][] {{0,1},{0,-1},{1,0},{-1,0}};
+
+        int steps = 0;
+        int x, y;
+        while (!queue.isEmpty()) {
+            steps++;
+
+            int n = queue.size();
+            for (int i = 0; i < n; i++) {
+                int[] current = queue.poll();
+
+                for (int[] direction : directions) {
+                    x = current[0] + direction[0];
+                    y = current[1] + direction[1];
+
+                    if (x < 0 || x >= rows || y < 0 || y >= columns) continue;
+                    if (maze[x][y] == '+') continue;
+
+                    if (x == 0 || x == rows - 1 || y == 0 || y == columns - 1) return steps;
+
+                    maze[x][y] = '+';
+                    queue.offer(new int[]{x, y});
+                }
+            }
+        }
+        return -1;
+    }
+}
